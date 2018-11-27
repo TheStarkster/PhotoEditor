46a0e65eb1e563bff5106986148e5a020ca1f545
Gabor Fekete <gabor@zedge.net>
2018-11-27 10:50:59 +0200

    Reduce memory usage when capturing image from OpenGL.

--- photoeditor/src/main/java/ja/burhanrashid52/photoeditor/BitmapUtil.java
+++ photoeditor/src/main/java/ja/burhanrashid52/photoeditor/BitmapUtil.java
@@ -6,8 +6,6 @@ import android.opengl.GLES20;
 import android.opengl.GLException;
 import android.opengl.GLSurfaceView;
 
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
 import java.nio.IntBuffer;
 
 import javax.microedition.khronos.opengles.GL10;
@@ -82,43 +80,78 @@ public class BitmapUtil {
      * @throws OutOfMemoryError error when system is out of memory to load and save bitmap
      */
     public static Bitmap createBitmapFromGLSurface(GLSurfaceView glSurfaceView, GL10 gl) throws OutOfMemoryError {
-        int x = 0, y = 0;
         int w = glSurfaceView.getWidth();
         int h = glSurfaceView.getHeight();
         int bitmapBuffer[] = new int[w * h];
-        int bitmapSource[] = new int[w * h];
         IntBuffer intBuffer = IntBuffer.wrap(bitmapBuffer);
         intBuffer.position(0);
-
         try {
-            gl.glReadPixels(x, y, w, h, GL10.GL_RGBA, GL10.GL_UNSIGNED_BYTE, intBuffer);
-            int offset1, offset2;
-            for (int i = 0; i < h; i++) {
-                offset1 = i * w;
-                offset2 = (h - i - 1) * w;
-                for (int j = 0; j < w; j++) {
-                    int texturePixel = bitmapBuffer[offset1 + j];
-                    int blue = (texturePixel >> 16) & 0xff;
-                    int red = (texturePixel << 16) & 0x00ff0000;
-                    int pixel = (texturePixel & 0xff00ff00) | red | blue;
-                    bitmapSource[offset2 + j] = pixel;
-                }
-            }
+            gl.glReadPixels(0, 0, w, h, GL10.GL_RGBA, GL10.GL_UNSIGNED_BYTE, intBuffer);
+            convertRgbaToArgbAndFlip(bitmapBuffer, w, h);
         } catch (GLException e) {
             return null;
         }
-        return Bitmap.createBitmap(bitmapSource, w, h, Bitmap.Config.ARGB_8888);
+        return Bitmap.createBitmap(bitmapBuffer, w, h, Bitmap.Config.ARGB_8888);
     }
 
     public static Bitmap createBitmapFromGlFrameBuffer(int x, int y, int width, int height) throws OutOfMemoryError {
-        ByteBuffer buffer = ByteBuffer.allocateDirect(width * height * 4);
-        buffer.order(ByteOrder.LITTLE_ENDIAN);
-        buffer.rewind();
-        GLES20.glReadPixels(x, y, width, height, GLES20.GL_RGBA, GLES20.GL_UNSIGNED_BYTE, buffer);
-        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-        buffer.rewind();
-        bitmap.copyPixelsFromBuffer(buffer);
-        return bitmap;
+        int bitmapBuffer[] = new int[width * height];
+        IntBuffer intBuffer = IntBuffer.wrap(bitmapBuffer);
+        intBuffer.position(0);
+        try {
+            GLES20.glReadPixels(x, y, width, height, GL10.GL_RGBA, GL10.GL_UNSIGNED_BYTE, intBuffer);
+            convertRgbaToArgb(bitmapBuffer);
+        } catch (GLException e) {
+            return null;
+        }
+        return Bitmap.createBitmap(bitmapBuffer, width, height, Bitmap.Config.ARGB_8888);
+    }
+
+    private static void convertRgbaToArgbAndFlip(int []pixels, final int w, final int h) {
+        int topHalfOffset;
+        int bottomHalfOffset;
+        for (int i = 0; i < (h / 2); ++i) {
+            topHalfOffset = i * w;
+            bottomHalfOffset = (h - i - 1) * w;
+            for (int j = 0; j < w; ++j) {
+                int topRgbaPixel = pixels[topHalfOffset + j];
+                int bottomRgbaPixel = pixels[bottomHalfOffset + j];
+                int topArgbPixel =
+                        (topRgbaPixel & 0xff00ff00) // green
+                                | ((topRgbaPixel << 16) & 0x00ff0000) // red
+                                | ((topRgbaPixel >> 16) & 0xff); // blue
+                int bottomArgbPixel =
+                        (bottomRgbaPixel & 0xff00ff00) // green
+                                | ((bottomRgbaPixel << 16) & 0x00ff0000) // red
+                                | ((bottomRgbaPixel >> 16) & 0xff); // blue
+                pixels[topHalfOffset + j] = bottomArgbPixel;
+                pixels[bottomHalfOffset + j] = topArgbPixel;
+            }
+        }
+
+        if ((h % 2) != 0) {
+            int middleRowOffset = (h / 2) * w;
+            for (int j = 0; j < w; ++j) {
+                int rgbaPixel = pixels[middleRowOffset + j];
+                int argbPixel =
+                        (rgbaPixel & 0xff00ff00) // green
+                                | ((rgbaPixel << 16) & 0x00ff0000) // red
+                                | ((rgbaPixel >> 16) & 0xff); // blue
+                pixels[middleRowOffset + j] = argbPixel;
+            }
+        }
+    }
+
+    private static void convertRgbaToArgb(int []pixels) {
+        final int len = pixels.length;
+        for (int i = 0; i < len; ++i) {
+            int rgbaPixel = pixels[i];
+            int topArgbPixel =
+                    (rgbaPixel & 0xff00ff00) // green
+                            | ((rgbaPixel << 16) & 0x00ff0000) // red
+                            | ((rgbaPixel >> 16) & 0xff); // blue
+            pixels[i] = topArgbPixel;
+        }
     }
 
 }
